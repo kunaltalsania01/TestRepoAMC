d92c143caf6e36270266d6605d48d04b2db9f4dd
kunaltalsania01 <talsania.kunal@gmail.com>
2020-01-13 16:53:40 +0530

    Add Vikas Lan Controller and lan class

---
+++ TestAMC/Models/Lan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestAMC.Models
+{
+    public class Lan
+    {
+        public string Name { get; set; }
+        public  string Address { get; set; }
+    }
+}
