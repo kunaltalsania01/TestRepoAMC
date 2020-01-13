0a740a75d90009b5b0b40d546d06a7492a332d36
kunaltalsania01 <talsania.kunal@gmail.com>
2020-01-13 15:49:18 +0530

    Kunal Add New controller and health class

---
+++ TestAMC/Controllers/HelthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestAMC.Controllers
+{
+    public class HelthController : Controller
+    {
+        // GET: Helth
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
