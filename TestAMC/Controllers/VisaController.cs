45992564d26298fa74475ea16c7c60c0caed5756
kunaltalsania01 <talsania.kunal@gmail.com>
2020-01-13 15:34:01 +0530

    Add Kunal New Visa Controller

---
+++ TestAMC/Controllers/VisaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestAMC.Controllers
+{
+    public class VisaController : Controller
+    {
+        // GET: Visa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
