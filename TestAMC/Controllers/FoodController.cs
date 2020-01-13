ed6ec2fb3384c47a875cc0b6d4ba93ee667eba06
kunaltalsania01 <talsania.kunal@gmail.com>
2020-01-13 15:43:52 +0530

    Add Vikash Food Controller

---
+++ TestAMC/Controllers/FoodController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestAMC.Controllers
+{
+    public class FoodController : Controller
+    {
+        // GET: Food
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
